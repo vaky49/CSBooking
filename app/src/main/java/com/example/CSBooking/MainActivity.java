53cb272f4f5ea24ab7d93c534e03919b5ec74b85
vivek <vaky49@gmail.com>
2019-03-19 22:55:02 +0100

    Cleaned up extra code

--- app/src/main/java/com/example/CSBooking/MainActivity.java
+++ app/src/main/java/com/example/CSBooking/MainActivity.java
@@ -7,14 +7,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mDriver, mCustomer;
+    private Button mDriver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mDriver = (Button) findViewById(R.id.driver);
-        mCustomer = (Button) findViewById(R.id.customer);
 
         startService(new Intent(MainActivity.this, onAppKilled.class));
         mDriver.setOnClickListener(new View.OnClickListener() {
@@ -27,14 +26,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mCustomer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CustomerLoginActivity.class);
-                startActivity(intent);
-                finish();
-                return;
-            }
-        });
     }
 }
